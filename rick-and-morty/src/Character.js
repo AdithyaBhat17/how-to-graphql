01d06293fb284c63ab9fd68d01eaf0e344356fa7
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-01-16 17:40:55 +0530

    🎨 render characterlist

---
+++ rick-and-morty/src/Character.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default class Character extends React.Component{
+    render(){
+        const { id, name, image } = this.props
+        return(
+            <Link className="col-md-2 col-xs-6" to={`/character/${id}`}>
+                <div>
+                    <div className="thumbnail">
+                        <img src={image} alt={name}/>
+                    </div>
+                    <p className="char-link"><span>{name}</span> <span> ></span></p>
+                </div>
+            </Link>
+            
+        )
+    }
+}
