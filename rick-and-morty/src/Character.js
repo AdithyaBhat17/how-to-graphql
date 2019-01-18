425218b5f41385862ec80b8e7ad6bfd3131afb80
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-01-18 13:50:18 +0530

    🆕  Added navbar

--- rick-and-morty/src/Character.js
+++ rick-and-morty/src/Character.js
@@ -5,7 +5,7 @@ export default class Character extends React.Component{
     render(){
         const { id, name, image } = this.props
         return(
-            <Link className="col-md-2 col-xs-6" to={`/character/${id}`}>
+            <Link className="col-md-2 col-6" to={`/character/${id}`}>
                 <div>
                     <div className="thumbnail">
                         <img src={image} alt={name}/>
