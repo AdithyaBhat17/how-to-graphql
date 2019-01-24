c36fac2cb4b090a6bea5f9fdc774a9fb38018209
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-01-24 19:23:28 +0530

    :sparkles: Added route for episodes

--- rick-and-morty/src/Routes.js
+++ rick-and-morty/src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from  'react-router-dom'
 
 import App from './App'
 import CharacterDetails from './CharacterDetails'
+import Episode from './Episode';
 
 export default class Routes extends React.Component{
     render(){
@@ -11,6 +12,7 @@ export default class Routes extends React.Component{
                 <Fragment>
                     <Route exact path="/" component={App}/>
                     <Route exact path="/character/:id" component={CharacterDetails} />
+                    <Route exact path="/episodes" component={Episode} />
                 </Fragment>
             </Router>
         )
